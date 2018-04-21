a7e9dc6dd8c4d541e1e86b72f6b9a2bb302d538f
reimx007 <carlreim@gmail.com>
2018-04-21 16:37:15 -0500

    geojson_Scratch

---
+++ app/script.js
@@ -0,0 +1,28 @@
+// Initialize the map on the "map" div with a given center and zoom
+var map = L.map('map', {
+    center: [44.969219, -93.262510],
+    zoom: 12
+});
+
+
+/* Pull in basemap layers from
+/* Basemap #1: Esri Dark Gray */
+var EsriDarkGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	maxZoom: 16
+}).addTo(map);
+
+/* Basemap #2: Esri Dark Gray Reference */
+var EsriDarkGrayCanvasRef = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Reference/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	maxZoom: 16
+}).addTo(map);
+
+var mnCountiesJSON = new L.GeoJSON.AJAX("data/mnCounties.geojson", {
+  style: function() {
+          return {
+            color: "#fff",
+            weight: 2
+          };
+  }
+}).addTo(map);
